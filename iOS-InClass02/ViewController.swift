6f27180f187628369c18f5d479490eb0780ee9f9
Alay Chaniyara <alaychaniyara@gmail.com>
2018-10-26 15:40:06 -0400

    Initial Commit

---
+++ iOS-InClass02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-InClass02
+//
+//  Created by Alay Chaniyara on 10/26/18.
+//  Copyright © 2018 Alay Chaniyara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iOS-InClass02Tests/iOS_InClass02Tests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_InClass02Tests.swift
+//  iOS-InClass02Tests
+//
+//  Created by Alay Chaniyara on 10/26/18.
+//  Copyright © 2018 Alay Chaniyara. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_InClass02
+
+class iOS_InClass02Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
